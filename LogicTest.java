68b906ec922ac01fef1c53a27c4d22b6a27b1601
tgqiang <tayguoqiang1993@gmail.com>
2016-02-22 17:12:50 +0800

    Initializing base Java files

---
+++ LogicTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.runners.MethodSorters;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+class LogicTest {
+
+}
