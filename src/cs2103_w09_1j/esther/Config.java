13533a903b8498a94cf29a190a8ad95974134f1a
HuiShan <a0126000@u.nus.edu>
2016-03-27 04:40:39 +0800

    Complete dateparser and error checking on add command
    config just simply include the start and end

---
+++ src/cs2103_w09_1j/esther/Config.java
@@ -0,0 +1,212 @@
+package cs2103_w09_1j.esther;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Config {
+
+	private int referenceID;
+	private Path savePath;
+	private HashMap<String, String> fieldNameAliases;
+
+	private static final int defaultReferenceID = 0;
+	private static final Path defaultSavePath = Paths.get("esther.txt");
+	private static final String[][] defaultFieldNameAliases = {	{ "taskName", "taskName" },
+																{ "tName", "taskName" },
+																{ "name", "taskName" },
+																{ "nm", "taskName" },
+																{ "n", "taskName" },
+																{ "startDate", "startDate" },
+																{ "startdate", "startDate" },
+																{ "sd", "startDate" },
+																{ "startd", "startDate" },
+																{ "endDate", "endDate" },
+																{ "endd", "endDate"},
+																{ "enddate", "endDate"},
+																{ "ed", "endDate"},
+																{ "startTime", "startTime"},
+																{ "starttime", "startTime"},
+																{ "startt", "startTime"},
+																{ "st", "startTime"},
+																{ "endTime", "endTime"},
+																{ "endtime", "endTime"},
+																{ "endt", "endTime"},
+																{ "et", "endTime"},
+																{ "id", "taskID" },
+																{ "taskid", "taskID" },
+																{ "priority", "priority" },
+																{ "prio", "priority" },
+																{ "pri", "priority" },
+																{ "pr", "priority" },
+																{ "p", "priority" },
+																{ "completed", "complete" },
+																{ "complete", "complete" },
+																{ "comp", "complete" },
+																{ "cp", "complete" },
+																{ "done", "complete" },
+																{ "dn", "complete" } };
+
+	private static final String[] attributeNames = {
+														"ReferenceID", "SaveLocation",
+														"FieldNameAliases" };
+	private static final String attributeFormat = "%1$s = %2$s;\n";
+	private static final String attributeRegex = " = ([^;]+);";
+	private static final String fieldNameRegex = "([\\w]+) = ([\\w]+);\n";
+
+	/**
+	 * Constructor for default config
+	 */
+	public Config() {
+		setReferenceID(getDefaultReferenceID());
+		setSavePath(getDefaultSavePath());
+		setFieldNameAliases(constructDefaultFieldNameAliases());
+	}
+
+	/**
+	 * Constructor for config given string input
+	 * 
+	 * @param configString
+	 *            String containing information for config construction
+	 * @throws Exception 
+	 */
+	public Config(String configString) throws ParseException {
+		this();
+		String[] resultsArray = new String[2];
+		for (int i = 0; i < 2; i++) {
+			resultsArray[i] = findMatch(attributeNames[i], configString);
+			if (resultsArray[i] == null) {
+				throw new ParseException("Config file load failed", i);
+			} else {
+				configString = configString.replaceFirst(attributeNames[i] + attributeRegex, "");
+			}
+		}
+		
+		setReferenceID(Integer.parseInt(resultsArray[0]));
+		setSavePath(Paths.get(resultsArray[1]));
+		
+		Matcher fieldNameMatcher = Pattern.compile(fieldNameRegex).matcher(configString);
+		while(fieldNameMatcher.find()){
+			fieldNameAliases.put(fieldNameMatcher.group(1), fieldNameMatcher.group(2));
+		}
+	}
+
+	/**
+	 * 
+	 */
+	public String toString() {
+		String configStr = "";
+		configStr += String.format(	attributeFormat,
+									attributeNames[0],
+									String.valueOf(getReferenceID()));
+		configStr += String.format(	attributeFormat,
+									attributeNames[1],
+									getSavePath().toString());
+		configStr += "\n";
+		configStr += attributeNames[2] + ":\n";
+		configStr += printHashMap(getFieldNameAliases());
+		return configStr;
+	}
+
+	/**
+	 * 
+	 * @param hashMap
+	 * @return
+	 */
+	public String printHashMap(HashMap<String, String> hashMap) {
+		String hashMapString = "";
+		Iterator<HashMap.Entry<String, String>> it = hashMap.entrySet().iterator();
+		while (it.hasNext()) {
+			HashMap.Entry<String, String> pair = (HashMap.Entry<String, String>) it.next();
+			hashMapString += pair.getKey() + " = " + pair.getValue() + ";\n";
+		}
+		return hashMapString;
+	}
+
+	private String findMatch(String regex, String input) {
+		return Task.findMatch(regex + attributeRegex, input);
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private HashMap<String, String> constructDefaultFieldNameAliases() {
+		HashMap<String, String> fieldNameAliases = new HashMap<>();
+		for (int i = 0; i < getDefaultFieldNameAliases().length; i++) {
+			fieldNameAliases.put(	getDefaultFieldNameAliases()[i][0],
+									getDefaultFieldNameAliases()[i][1]);
+		}
+		return fieldNameAliases;
+	}
+
+	/**
+	 * @return the referenceID
+	 */
+	public int getReferenceID() {
+		return referenceID;
+	}
+
+	/**
+	 * @param referenceID
+	 *            the referenceID to set
+	 */
+	public void setReferenceID(int referenceID) {
+		this.referenceID = referenceID;
+	}
+
+	/**
+	 * @return the fieldNameAliases
+	 */
+	public HashMap<String, String> getFieldNameAliases() {
+		return fieldNameAliases;
+	}
+
+	/**
+	 * @param fieldNameAliases
+	 *            the fieldNameAliases to set
+	 */
+	public void setFieldNameAliases(HashMap<String, String> fieldNameAliases) {
+		this.fieldNameAliases = fieldNameAliases;
+	}
+
+	/**
+	 * @return the saveLocation
+	 */
+	public Path getSavePath() {
+		return savePath;
+	}
+
+	/**
+	 * @param saveLocation
+	 *            the saveLocation to set
+	 */
+	public void setSavePath(Path saveLocation) {
+		this.savePath = saveLocation;
+	}
+
+	/**
+	 * @return the defaultReferenceID
+	 */
+	private int getDefaultReferenceID() {
+		return defaultReferenceID;
+	}
+
+	/**
+	 * @return the defaultSaveLocation
+	 */
+	private Path getDefaultSavePath() {
+		return defaultSavePath;
+	}
+
+	/**
+	 * @return the defaultFieldNameAliases
+	 */
+	private String[][] getDefaultFieldNameAliases() {
+		return defaultFieldNameAliases;
+	}
+}
--- src/cs2103_w09_1j/esther/DateParserTest.java
+++ src/cs2103_w09_1j/esther/DateParserTest.java
@@ -6,11 +6,10 @@ import java.util.Date;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.ocpsoft.prettytime.format.SimpleTimeFormat;
 
 public class DateParserTest {
 
-	DateParser dp;
+	DateParser dateParser;
 	String input = "";
 	String date = "";
 	String time = "";
@@ -20,236 +19,133 @@ public class DateParserTest {
 
 	@Before
 	public void beforeRun() {
-		dp = new DateParser();
+		this.dateParser=new DateParser();
 	}
 
 	@Test
-	public void dateFormatTest() throws ParseException {
+	public void dateFormatTest() throws ParseException, InvalidInputException {
 		input = "12/02/2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd/MM/yyyy: ", "dd/MM/yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format dd/MM/yyyy: ", "12/02/2015", date);
 
 		input = "12.02.2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd.MM.yyyy: ", "dd.MM.yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format dd.MM.yyyy: ", "12/02/2015", date);
 
 		input = "12-02-2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd-MM-yyyy: ", "dd-MM-yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format dd-MM-yyyy: ", "12/02/2015", date);
 
 		input = "12 02 2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd MM yyyy: ", "dd MM yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format dd MM yyyy: ", "12/02/2015", date);
 
 		input = "12022015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format ddMMyyyy: ", "ddMMyyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format ddMMyyyy: ", "12/02/2015", date);
 
 		input = "12 Feb 2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd MMM yyyy: ", "dd MMM yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format dd MMM yyyy: ", "12/02/2015", date);
 
 		input = "12 february 2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd MMM yyyy: ", "dd MMM yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format dd MMM yyyy: ", "12/02/2015", date);
 
-		input = "12feb2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format ddMMMyyyy: ", "ddMMMyyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
-		Assert.assertEquals("For Format ddMMMyyyy: ", "12/02/2015", date);
-
-		input = "12FEBRUARY 2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format ddMMMyyyy: ", "ddMMMyyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
-		Assert.assertEquals("For Format ddMMMyyyy: ", "12/02/2015", date);
+		input = "12feb 2015";
+		date = dateParser.getDateTime(input)[0];
+		Assert.assertEquals("For Format ddMMM yyyy: ", "12/02/2015", date);
 
 		input = "12 february, 2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd MMM, yyyy: ", "dd MMM, yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format dd MMM, yyyy: ", "12/02/2015", date);
 
 		input = "12 FEB, 2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd MMM, yyyy: ", "dd MMM, yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format dd MMM, yyyy: ", "12/02/2015", date);
 
-		input = "Feb 12 2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format MMM dd yyyy: ", "MMM dd yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
-		Assert.assertEquals("For Format MMM dd yyyy: ", "12/02/2015", date);
-
 		input = "Feb 12, 2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format MMM dd, yyyy: ", "MMM dd, yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format MMM dd, yyyy: ", "12/02/2015", date);
 
-		input = "february12 2015";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format MMMdd yyyy: ", "MMMdd yyyy", dateFormat);
-		date = dp.getDate(input, dateFormat);
-		Assert.assertEquals("For Format MMMdd yyyy: ", "12/02/2015", date);
-
 		input = "12/02";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd/MM: ", "dd/MM", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format dd/MM: ", "12/02/2017", date);
 
-		input = "12.02";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd.MM: ", "dd.MM", dateFormat);
-		date = dp.getDate(input, dateFormat);
-		Assert.assertEquals("For Format dd.MM: ", "12/02/2017", date);
-
 		input = "12 feb";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format dd MMM: ", "dd MMM", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format dd MMM: ", "12/02/2017", date);
 
 		input = "12FEBRUARY";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format ddMMM: ", "ddMMM", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format ddMMM: ", "12/02/2017", date);
 
 		input = "feb 12";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format MMM dd: ", "MMM dd", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format MMM dd: ", "12/02/2017", date);
 
 		input = "feb12";
-		dateFormat = dp.getDateFormat(input);
-		Assert.assertSame("For Format MMMdd: ", "MMMdd", dateFormat);
-		date = dp.getDate(input, dateFormat);
+		date = dateParser.getDateTime(input)[0];
 		Assert.assertEquals("For Format MMMdd: ", "12/02/2017", date);
 	}
 
 	@Test
-	public void timeFormatTest() throws ParseException {
+	public void timeFormatTest() throws ParseException, InvalidInputException {
 		input = "03:00pm";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format hh:mma : ", "hh:mma", timeFormat);
-		time = dp.getTime(input, timeFormat);
+		time = dateParser.getDateTime(input)[1];
 		Assert.assertEquals("For Format hh:mma : ", "15:00", time);
 
 		input = "03:00 PM";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format hh:mm a : ", "hh:mm a", timeFormat);
-		time = dp.getTime(input, timeFormat);
+		time = dateParser.getDateTime(input)[1];
 		Assert.assertEquals("For Format hh:mm a : ", "15:00", time);
 
-		input = "03-00PM";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format hh-mma : ", "hh-mma", timeFormat);
-		time = dp.getTime(input, timeFormat);
-		Assert.assertEquals("For Format hh-mma : ", "15:00", time);
-
-		input = "3-00 pm";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format hh-mm a : ", "hh-mm a", timeFormat);
-		time = dp.getTime(input, timeFormat);
-		Assert.assertEquals("For Format hh-mm a : ", "15:00", time);
-
 		input = "0300pm";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format hhmma : ", "hhmma", timeFormat);
-		time = dp.getTime(input, timeFormat);
+		time = dateParser.getDateTime(input)[1];
 		Assert.assertEquals("For Format hhmma : ", "15:00", time);
 
+		System.out.println(1);
 		input = "0300 pm";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format hhmm a : ", "hhmm a", timeFormat);
-		time = dp.getTime(input, timeFormat);
+		time = dateParser.getDateTime(input)[1];
 		Assert.assertEquals("For Format hhmm a : ", "15:00", time);
 
+		System.out.println(2);
 		input = "03:00";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format HH:mm : ", "HH:mm", timeFormat);
-		time = dp.getTime(input, timeFormat);
+		time = dateParser.getDateTime(input)[1];
 		Assert.assertEquals("For Format HH:mm : ", "03:00", time);
 
+		System.out.println(3);
 		input = "15:00";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format HH:mm : ", "HH:mm", timeFormat);
-		time = dp.getTime(input, timeFormat);
+		time = dateParser.getDateTime(input)[1];
 		Assert.assertEquals("For Format HH:mm : ", "15:00", time);
 
-		input = "15-00";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format HH-mm : ", "HH-mm", timeFormat);
-		time = dp.getTime(input, timeFormat);
-		Assert.assertEquals("For Format HH-mm : ", "15:00", time);
-
-		input = "15.00";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format HH.mm : ", "HH.mm", timeFormat);
-		time = dp.getTime(input, timeFormat);
-		Assert.assertEquals("For Format HH.mm : ", "15:00", time);
-
 		input = "1500";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format HHmm : ", "HHmm", timeFormat);
-		time = dp.getTime(input, timeFormat);
+		time = dateParser.getDateTime(input)[1];
 		Assert.assertEquals("For Format HHmm : ", "15:00", time);
 
 		input = "03pm";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format hha : ", "hha", timeFormat);
-		time = dp.getTime(input, timeFormat);
+		time = dateParser.getDateTime(input)[1];
 		Assert.assertEquals("For Format HHa : ", "15:00", time);
 
 		input = "3 PM";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format hh a : ", "hh a", timeFormat);
-		time = dp.getTime(input, timeFormat);
+		time = dateParser.getDateTime(input)[1];
 		Assert.assertEquals("For Format HH a : ", "15:00", time);
 
 		input = "15";
-		timeFormat = dp.getTimeFormat(input, "");
-		Assert.assertSame("For Format HH : ", "HH", timeFormat);
-		time = dp.getTime(input, timeFormat);
+		time = dateParser.getDateTime(input)[1];
 		Assert.assertEquals("For Format HH : ", "15:00", time);
 
 	}
 
 	@Test
-	public void dateTimeFormatTest() throws ParseException {
-		SimpleDateFormat defaultDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		SimpleDateFormat defaultTimeFormat = new SimpleDateFormat("HH:mm");
+	public void dateTimeFormatTest() throws ParseException, InvalidInputException {
 
-		input = "February 12 2013 3pm";
-		dateFormat = dp.getDateFormat(input);
-		timeFormat = dp.getTimeFormat(input, dateFormat);
-		givenDate = dp.getDateTime(input, dateFormat + " " + timeFormat);
-		Assert.assertEquals("For Date:", "12/02/2013", givenDate[0]);
+		input = "February 12 3pm";
+		givenDate = dateParser.getDateTime(input);
+		Assert.assertEquals("For Date:", "12/02/2017", givenDate[0]);
 		Assert.assertEquals("For Time:", "15:00", givenDate[1]);
 		
 		input= "12/02/2013 3";
-		dateFormat = dp.getDateFormat(input);
-		timeFormat = dp.getTimeFormat(input, dateFormat);
-		givenDate = dp.getDateTime(input, dateFormat + " " + timeFormat);
+		givenDate = dateParser.getDateTime(input);
 		Assert.assertEquals("For Date:", "12/02/2013", givenDate[0]);
 		Assert.assertEquals("For Time:", "03:00", givenDate[1]);
 	}
