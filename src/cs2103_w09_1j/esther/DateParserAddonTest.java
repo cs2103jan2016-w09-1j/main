92594edf571992d650d74882d8b48eca1aa5909e
Jeremy Hon <jeremy.hon.gy@gmail.com>
2016-03-29 10:11:17 +0800

    Restructure tests

--- src/cs2103_w09_1j/esther/DateParserAddonTest.java
+++ src/cs2103_w09_1j/esther/DateParserAddonTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 
 import org.junit.Test;
@@ -14,45 +15,12 @@ public class DateParserAddonTest {
 	SimpleDateFormat defaultDateFormat = new SimpleDateFormat(defaultDateFormatPattern);
 	Calendar today = Calendar.getInstance();
 	
-	@Test
-	public void calTest(){
-		Calendar calendar = Calendar.getInstance();
-		calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
-	}
 	
 	@Test
-	public void weekTest1(){
-		String result = dateParserAddon.findWordyDate(" this Tuesday");
-		assertNotEquals("result was null",result,null);
-		if(result != null){
-			try {
-				Date date = defaultDateFormat.parse(result);
-				Calendar calendar = dateToCalendar(date);
-				assertTrue(calendar.get(Calendar.DAY_OF_WEEK) == Calendar.TUESDAY);
-				assertTrue(calendar.compareTo(today) > 0);
-			} catch (ParseException e) {
-				fail();
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	@Test
-	public void weekTest2(){
-		String result = dateParserAddon.findWordyDate(" hello next wed");
-		assertNotEquals("result was null",result,null);
-		if(result != null){
-			try {
-				Date date = defaultDateFormat.parse(result);
-				Calendar calendar = dateToCalendar(date);
-				assertTrue(calendar.get(Calendar.DAY_OF_WEEK) == Calendar.WEDNESDAY);
-				assertTrue(calendar.compareTo(today) > 0);
-				assertEquals(calendar.get(Calendar.WEEK_OF_YEAR),today.get(Calendar.WEEK_OF_YEAR) + 1);
-			} catch (ParseException e) {
-				fail();
-				e.printStackTrace();
-			}
-		}
+	public void generalTest(){
+	    String test = "23/7/2016 0200";
+	    String[] result = dateParserAddon.find24HTime(test);
+	    System.out.println(Arrays.toString(result));
 	}
 	
 	public static Calendar dateToCalendar(Date date){ 
