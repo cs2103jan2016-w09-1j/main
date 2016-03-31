da5154396431f5b1dfaab1180ab6417c0cead0d8
Jeremy Hon <jeremy.hon.gy@gmail.com>
2016-03-31 17:31:10 +0800

    new test driver class

---
+++ src/EstherTestDriver.java
@@ -0,0 +1,19 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import cs2103_w09_1j.esther.DateParserTest;
+import cs2103_w09_1j.esther.TaskTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	EstherTest.class, 
+	LogicTest.class, 
+	ParserTest.class, 
+	DateParserTest.class,
+	TaskTest.class
+})
+
+public class EstherTestDriver {
+
+}
