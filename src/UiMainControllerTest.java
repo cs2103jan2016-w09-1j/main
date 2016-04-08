2f6cadaf544689a51397f3fbae9009d42c1d360c
Carl26 <carlguo26@gmail.com>
2016-04-08 21:50:16 +0800

    UiMainControllerTest stub

---
+++ src/UiMainControllerTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UiMainControllerTest {
+
+	@Test
+	public void testGetRes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOverdueClick() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpcomingClick() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnterAndNavigate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitialize() {
+		fail("Not yet implemented");
+	}
+
+}
