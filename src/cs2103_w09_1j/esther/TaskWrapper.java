f37990a401314a27e58826e5c2d40b483b18e1f5
tgqiang <tayguoqiang1993@gmail.com>
2016-04-11 11:29:11 +0800

    Removed printline statements in TaskWrapper.java

--- src/cs2103_w09_1j/esther/TaskWrapper.java
+++ src/cs2103_w09_1j/esther/TaskWrapper.java
@@ -15,23 +15,15 @@ public class TaskWrapper {
 	public TaskWrapper(Task task) {
 		record = task;
 		taskName.set(task.getName());
-		System.out.println("Task name is " + task.getName());
 		id.set(Integer.toString(task.getId()));
-		System.out.println("Task id is " + task.getId());
 		priority.set(Integer.toString(task.getPriority()));
-		System.out.println("Task priority is " + task.getPriority());
 		endDate.set(task.eDateToString());
-		System.out.println("Task endDate is " + task.getEndDate());
 		
 		if (!task.sDateToString().isEmpty()) {
-			System.out.println("Task has start date!");
 			startDate.set(task.sDateToString());
-			System.out.println("Task start date is " + task.getStartDate());
 			date.set("from " + startDate.get() + " to " + endDate.get());
-			System.out.println("Task date is " + date.get());
 		} else {
 			date.set(task.eDateToString());
-			System.out.println("Task date is " + date.get());
 		}
 	}
 
